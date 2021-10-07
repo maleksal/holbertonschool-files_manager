ac12f2f2f2adb3eaf63700de1b1997ca2794b702
malek salem <malek.salem.14@gmail.com>
2021-10-07 22:26:26 +0100

    add to redij.js

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import {createClient} from 'redis';
+const { promisify } = require('util');
+
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.get = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) { 
+    return this.client.get(key)
+  }
+
+  async set(key, value, expire) {
+    this.client.setex(key, duration, expire);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
