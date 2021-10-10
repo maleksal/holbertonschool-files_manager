ee391034dc57161ce72cc3d2e88f530d51c465cf
malek salem <malek.salem.14@gmail.com>
2021-10-10 21:56:23 +0100

    add tests for user auth

---
+++ tests/test_auth.js
@@ -0,0 +1,15 @@
+# Returns auth token
+echo "trying Connect"
+curl 0.0.0.0:5000/connect -H "Authorization: Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=" ; echo ""
+
+# returns user info
+echo "trying users/me"
+curl 0.0.0.0:5000/users/me -H "X-Token: 031bffac-3edc-4e51-aaae-1c121317da8a" ; echo ""
+
+# Disconnect
+echo "trying disconnect"
+curl 0.0.0.0:5000/disconnect -H "X-Token: 031bffac-3edc-4e51-aaae-1c121317da8a" ; echo ""
+
+echo "trying users/me"
+# returns error Unauthorized
+curl 0.0.0.0:5000/users/me -H "X-Token: 031bffac-3edc-4e51-aaae-1c121317da8a" ; echo ""
