625c75d0a7aa20d692dbd8d2be9d9264d9d084a0
malek salem <malek.salem.14@gmail.com>
2021-10-10 18:06:43 +0100

    add new endpoint to router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.Status);
 router.get('/stats', AppController.Stats);
+router.post('/users', UserController.postNew);
 
 export default router;
