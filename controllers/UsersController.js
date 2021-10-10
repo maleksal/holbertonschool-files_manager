8192815ec53ff5e562a2b98502e21025afe06dde
malek salem <malek.salem.14@gmail.com>
2021-10-10 18:06:29 +0100

    add user creation endpoint

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UserController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const exists = await dbClient.userExists(email);
+    if (exists) return res.status(400).json({ error: 'Already exist' });
+
+    const newUser = {
+      email,
+      password: sha1(password),
+    };
+
+    return res.status(201).json({
+      id: await dbClient.addUser(newUser),
+      email,
+    });
+  }
+}
+
+export default UserController;
