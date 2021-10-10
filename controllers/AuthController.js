ecdea4123142f26299cd9a0ef643801b425037ad
malek salem <malek.salem.14@gmail.com>
2021-10-10 21:55:45 +0100

    implement auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,43 @@
+import dbClient from '../utils/db';
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import RedisClient from '../utils/redis';
+
+
+class AuthController {
+
+    static async connect(req, res){
+
+        const { authorization } = req.headers;
+        if (!authorization) return res.status(401).json({ error: 'Unauthorized' });
+        
+        if (!(authorization.slice(0, 6) === 'Basic '))
+            if (!isAuthorized) return res.status(401).json({ error: 'Unauthorized' });
+
+        const authData = Buffer.from(authorization.slice(6), 'base64').toString('utf-8');
+        const email = authData.split(':')[0];
+        const pass = authData.split(':')[1];
+        
+        if (!email || !pass) return res.status(401).json({ error: 'Unauthorized' });
+
+        const user = await dbClient.userExists(email);
+        
+        if (!user) return res.status(401).json({ error: 'Unauthorized' });
+        if (!user.password || user.password !== sha1(pass)) return res.status(401).json({ error: 'Unauthorized' });
+                
+        const token = uuidv4();
+        RedisClient.set(`auth_${token}`, user._id.toString(), 86400);
+        return res.status(200).json({ token });
+    
+    }
+
+    static async disconnect(req, res) {
+        const redisUserKey = `auth_${req.token}`;
+        RedisClient.del(redisUserKey);
+    
+        return res.status(204).end();
+    
+    }
+}
+
+export default AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+
 
 const router = express.Router();
 
 router.get('/status', AppController.Status);
 router.get('/stats', AppController.Stats);
+
 router.post('/users', UserController.postNew);
+router.get('/users/me', UserController.me);
+
+router.get('/connect', AuthController.connect);
+router.get('/disconnect', AuthController.disconnect);
 
 export default router;
